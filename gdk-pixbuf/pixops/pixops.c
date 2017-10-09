d6cbbb9033b2430f33c84bc5bc610c7e77ebfa9f
Anselm Kruis <anselm.kruis@atos.net>
2017-10-09 10:54:36 +0200

    pixops: Fix oversight for CVE-2015-4491
    Backport the fix from commit 8dba67cb4 to 2.28.2

--- gdk-pixbuf/pixops/pixops.c
+++ gdk-pixbuf/pixops/pixops.c
@@ -1194,7 +1194,20 @@ make_filter_table (PixopsFilter *filter)
   int i_offset, j_offset;
   int n_x = filter->x.n;
   int n_y = filter->y.n;
-  int *weights = g_new (int, SUBSAMPLE * SUBSAMPLE * n_x * n_y);
+  gsize n_weights;
+  int *weights;
+
+  n_weights = SUBSAMPLE * SUBSAMPLE * n_x;
+  if (n_weights / (SUBSAMPLE * SUBSAMPLE) != n_x)
+    return NULL; /* overflow, bail */
+
+  n_weights *= n_y;
+  if (n_weights / (SUBSAMPLE * SUBSAMPLE * n_x) != n_y)
+    return NULL; /* overflow, bail */
+
+  weights = g_try_new (int, n_weights);
+  if (!weights)
+    return NULL; /* overflow, bail */
 
   for (i_offset=0; i_offset < SUBSAMPLE; i_offset++)
     for (j_offset=0; j_offset < SUBSAMPLE; j_offset++)
